1d178d6a9292f75aacdbf061ba9b2e956a5b8e8a
Prateek Mehta <prtk.mehta@gmail.com>
2024-04-14 01:04:04 -0400

    added VAE architecture plus train loop

--- code/models.py
+++ code/models.py
@@ -1,8 +1,54 @@
 # file to code model architectures
 
+import torch
+import torch.nn as nn
 
 # code VAE model architecture
 
-# code threhold based OOD detector 
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# VAE model
+class VAE(nn.Module):
+    def __init__(self, latent_size=100):
+        super(VAE, self).__init__()
+        self.latent_size = latent_size
+        self.encoder = nn.Sequential(
+            nn.Conv2d(3, 32, 4, 2, 1),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, 4, 2, 1),
+            nn.ReLU(),
+            nn.Conv2d(64, 128, 4, 2, 1),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(128 * 4 * 4, 2 * self.latent_size)
+        )
+        self.decoder = nn.Sequential(
+            nn.Linear(self.latent_size, 128 * 4 * 4),
+            nn.ReLU(),
+            nn.Unflatten(1, (128, 4, 4)),
+            nn.ConvTranspose2d(128, 64, 4, 2, 1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(64, 32, 4, 2, 1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(32, 3, 4, 2, 1),
+            nn.Sigmoid()
+        )
+
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        return mu + eps * std
+
+    def forward(self, x):
+        encoded = self.encoder(x)
+        mu, logvar = torch.split(encoded, self.latent_size, dim=1)
+        z = self.reparameterize(mu, logvar)
+        decoded = self.decoder(z)
+        return decoded, mu, logvar
+
+
+
+# code threshold based OOD detector 
 
 
--- code/utils.py
+++ code/utils.py
@@ -1,8 +1,67 @@
 # file to model utility functions
+import torch
+import numpy as np
 
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+                      
 # model train loop function
+def train_vae(model, optimizer, num_epochs, train_loader, test_loader, modelname):
+    train_loss = []
+    test_loss = []
+    num_train = 0
+    num_test = 0
+    best_loss = np.inf
 
-# model test loop function
+    for epoch in range(num_epochs):
+        # train mode
+        model.train()
+        avg_train_loss = 0
+        for i, (images, _) in enumerate(train_loader):
+            num_train += images.shape[0]
+            images = images.to(device)
+
+            # Forward pass
+            recon_images, mu, logvar = model(images)
+            loss = 0.5 * (recon_images - images).pow(2).sum() - 0.5 * (1 + logvar - mu.pow(2) - logvar.exp()).sum()
+            avg_train_loss += loss
+
+            # Backward and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        
+        avg_train_loss /= num_train          
+        
+        
+        # eval mode
+        model.eval()
+        avg_val_loss = 0
+        for i, (images, _) in enumerate(test_loader):
+            num_test += images.shape[0]
+            images = images.to(device)
+
+            # Forward pass
+            with torch.no_grad():
+                recon_images, mu, logvar = model(images)
+                loss = 0.5 * (recon_images - images).pow(2).sum() - 0.5 * (1 + logvar - mu.pow(2) - logvar.exp()).sum()
+                avg_val_loss += loss
+        
+        avg_val_loss /= num_test
+
+        # logging losses
+        train_loss.append(avg_train_loss)
+        test_loss.append(avg_train_loss)
+        print(f'Epoch [{epoch+1}/{num_epochs}], Training Loss: {avg_train_loss.item():.4f}, Validation Loss: {avg_val_loss.item():.4f}')
+
+        if avg_val_loss.item() < best_loss:
+            print(f'Best Validation Loss reduced from {best_loss:.4f} to {avg_val_loss.item():.4f}')
+            print("Saving Model")
+            best_loss = avg_val_loss.item()
+            name = r'../models/'+str(modelname)+'.pt'
+            torch.save(model.state_dict(), name)
+
+    return model, train_loss, test_loss
 
 # data loader
 
