93f1c252c13f3f8eeaf8ca7442c8b4e20a590c7e
Prateek Mehta <prtk.mehta@gmail.com>
2024-05-02 23:49:28 -0400

    added graphs

--- code/energy.py
+++ code/energy.py
@@ -2,6 +2,7 @@
 import torch
 import numpy as np
 from sklearn.metrics import roc_curve, auc, precision_recall_curve
+import matplotlib.pyplot as plt
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -117,6 +118,18 @@ class ELOOD:
         # AUROC (Area Under the Receiver Operating Characteristic Curve)
         fpr, tpr, _ = roc_curve(true_labels, predictions)
         auroc = auc(fpr, tpr)
+
+        # plot the roc curve for the model
+        plt.figure()
+        plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % auroc)
+        plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Receiver Operating Characteristic')
+        plt.legend(loc="lower right")
+        plt.show()
 
         return(false_positive_rate_95, aupr, auroc)
     
